8c266603bec5b69fcc605a7700fdb790d6bccc7b
Vladliena <sotnikovavladlena@gmail.com>
2023-07-15 22:32:16 +0300

    added comments to first_script.py

--- Week2/Day1_Python-Start/ExercisesXP/first_script.py
+++ Week2/Day1_Python-Start/ExercisesXP/first_script.py
@@ -1,25 +1,55 @@
-# Exercise 1
+# Exercise 1 : Hello World
+# Instructions
+# Print the following output in one line of code:
+
+# Hello world
+# Hello world
+# Hello world
+# Hello world
 
 print(("Hello world\n") * 4)
 
-# Exercise 2
+
+# Exercise 2 : Some Math
+# Instructions
+# Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8).
 
 print((99**3) * 8)
 
-# Exercise 3
 
-5 < 3 #false
-3 == 3 #true
-3 == "3" #false
-"3" > 3 #error
-"Hello" == "hello" #false
+# Exercise 3 : What Is The Output ?
+# Instructions
+# Predict the output of the following code snippets:
+# >>> 5 < 3
+# >>> 3 == 3
+# >>> 3 == "3"
+# >>> "3" > 3
+# >>> "Hello" == "hello"
 
-# Exercise 4
+5 < 3  # false
+3 == 3  # true
+3 == "3"  # false
+"3" > 3  # error
+"Hello" == "hello"  # false
+
+
+# Exercise 4 : Your Computer Brand
+# Instructions
+# Create a variable called computer_brand which value is the brand name of your computer.
+# Using the computer_brand variable print a sentence that states the following: "I have a <computer_brand> computer".
 
 computer_brand = "acer nitro 5"
 print("I have a {} computer".format(computer_brand))
 
-# Exercise 5
+
+# Exercise 5 : Your Information
+# Instructions
+# Create a variable called name, and set it’s value to your name.
+# Create a variable called age, and set it’s value to your age.
+# Create a variable called shoe_size, and set it’s value to your shoe size.
+# Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2 and 3.
+# Have your code print the info message.
+# Run your code
 
 name = "Vladlena"
 age = 30
@@ -33,7 +63,12 @@ print(info)
 info_option2 = f"My name is {name}, 2 days ago I turned {age} and my shoe size is {shoe_size} which is not very common and hard to find"
 print(info_option2)
 
-# Exercise 6
+
+# Exercise 6 : A & B
+# Instructions
+# Create two variables, a and b.
+# Each variable value should be a number.
+# If a is bigger then b, have your code print Hello World.
 
 a = 10
 b = 5
@@ -41,7 +76,10 @@ b = 5
 if a > b:
     print("Hello World")
 
-# Exercise 7
+
+# Exercise 7 : Odd Or Even
+# Instructions
+# Write code that asks the user for a number and determines whether this number is odd or even.
 
 number = int(input("write any number"))
 if number % 2 == 0:
@@ -49,19 +87,27 @@ if number % 2 == 0:
 else:
     print("number is odd")
 
-# Exercise 8
 
-name = input('Tell me your name')
+# Exercise 8 : What’s Your Name ?
+# Instructions
+# Write code that asks the user for their name and determines whether or not you have the same name, print out a funny message based on the outcome.
 
-if name == 'Vladlena':
-    print ('wow! we have the same name')
+name = input("Tell me your name")
+
+if name == "Vladlena":
+    print("wow! we have the same name")
 else:
-    print('my name is prettier')
+    print("my name is prettier")
 
-# Exercise 9
+
+# Exercise 9 : Tall Enough To Ride A Roller Coaster
+# Instructions
+# Write code that will ask the user for their height in inches.
+# If they are over 145cm print a message that states they are tall enough to ride.
+# If they are not tall enough print a message that says they need to grow some more to ride.
 
 height = int(input("tell me your height in inches"))
 if height > 145:
-    print('you are tall enough to ride')
+    print("you are tall enough to ride")
 else:
-    print('you need to grow more to ride')
+    print("you need to grow more to ride")
