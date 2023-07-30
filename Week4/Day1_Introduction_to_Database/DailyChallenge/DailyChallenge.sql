eeebc5da6bb1a4a138fc793d96787d5ba939e248
Vladliena <sotnikovavladlena@gmail.com>
2023-07-30 15:08:32 +0300

    Daily Challenge

---
+++ Week4/Day1_Introduction_to_Database/DailyChallenge/DailyChallenge.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- )
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES
+-- ('Matt','Damon','1970-10-08', 5),
+-- ('George','Clooney','1961-05-06', 2),
+-- ('Tom','Radle','1992-02-07', 1),
+-- ('Tom','Radle','1987-05-20', 4);
+
+-- select count(first_name) from actors
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES
+('John','Smith');
+-- the output is error
+
+
