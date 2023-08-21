32fbb56018e2baa85b10edd0a584a9119d0496a9
Vladliena <sotnikovavladlena@gmail.com>
2023-08-21 19:23:22 +0300

    uncomment code

--- Week7/Day2_Dom_Events/ExerciseXPAnimation/scripts.js
+++ Week7/Day2_Dom_Events/ExerciseXPAnimation/scripts.js
@@ -17,48 +17,83 @@ the interval will be cleared (ie. clearInterval) as soon as there will be 5 para
 
 
 //Part 1
-// let timer = setTimeout(alertWord,2000)
+let timer = setTimeout(alertWord,2000)
 
 
-// function alertWord(){
-//     alert('Hello World')
-// }
+function alertWord(){
+    alert('Hello World')
+}
 
 //Part 2
-// let timer = setTimeout(newp, 2000)
-// const containerMain = document.getElementById('container')
+let timer = setTimeout(newp, 2000)
+const containerMain = document.getElementById('container')
 
-// function newp() {
-//     containerMain.innerHTML = '<p>Hello World</p>'
-// }
+function newp() {
+    containerMain.innerHTML = '<p>Hello World</p>'
+}
 
 //Part 3
-// let timer = setInterval(newp, 2000);
-// const containerMain = document.getElementById('container');
-// const button = document.getElementById('clear');
-// button.addEventListener('click', function (){
-//     clearInterval(timer)
-// })
+let timer = setInterval(newp, 2000);
+const containerMain = document.getElementById('container');
+const button = document.getElementById('clear');
+button.addEventListener('click', function (){
+    clearInterval(timer)
+})
 
-// function newp() {
-//     const p = document.createElement('p')
-//     p.innerText = 'Hello World'
-//     containerMain.append(p)
-// }
+function newp() {
+    const p = document.createElement('p')
+    p.innerText = 'Hello World'
+    containerMain.append(p)
+}
 
 //Part 3.1
-// let timer = setInterval(newp, 2000);
-// const containerMain = document.getElementById('container');
+let timer = setInterval(newp, 2000);
+const containerMain = document.getElementById('container');
 
-// function newp() {
-//     const p = document.createElement('p')
-//     p.innerText = 'Hello World'
-//     containerMain.append(p)
-//     if (document.querySelectorAll('p').length >= 5) {
-//         clearInterval(timer)
-//     }
-// }
+function newp() {
+    const p = document.createElement('p')
+    p.innerText = 'Hello World'
+    containerMain.append(p)
+    if (document.querySelectorAll('p').length >= 5) {
+        clearInterval(timer)
+    }
+}
 
+
+// 🌟 Exercise 2 : Move The Box
+// Instructions
+//     < !DOCTYPE html >
+//         <html>
+//             <head>
+//                 <style>
+//                     #container {
+//                         width: 400px;
+//                     height: 400px;
+//                     position: relative;
+//                     background: yellow;
+//         }
+//                     #animate {
+//                         width: 50px;
+//                     height: 50px;
+//                     position: absolute;
+//                     background-color: red;
+//         }
+//                 </style>
+//             </head>
+//             <body>
+//                 <p><button onclick="myMove()">Click Me</button></p>
+//                 <div id="container">
+//                     <div id="animate"></div>
+//                 </div>
+//             </body>
+//         </html>
+
+
+// Copy the code above, to a structured HTML file.
+// In your Javascript file, use setInterval to move the < div id = "animate" > to the right side of the < div id = "container" >, when the button is clicked on.
+//     The < div id = "animate" > should move 1px to the right every milisecond, until it reaches the end of the < div id = "container" >.
+//         Hint : use clearInterval as soon as the box reaches the right end side of the container
+// Hint: check out the demonstration in the Course Noted named JS Functions
 
 const button = document.getElementsByTagName('button')[0];
 const div = document.getElementById('container')
