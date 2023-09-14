fc5c4eee537ff3f6d96ec00f02664a8199befa0b
Vladliena <sotnikovavladlena@gmail.com>
2023-09-14 08:43:21 +0300

    form

--- Week10/Day4/dailyChallenge/my-app/src/components/Form.js
+++ Week10/Day4/dailyChallenge/my-app/src/components/Form.js
@@ -1,5 +1,5 @@
-
 import { useState } from 'react'
+import './Form.css'
 
 
 
@@ -11,18 +11,20 @@ const Form = () => {
         age: "",
         gender: "",
         destination: "",
-        alergic: []
+        dietary_nuts: "",
+        dietary_lactose: "",
+        dietary_veganmeal: ""
     })
 
     const handleChange = (e) => {
-        const value = e.target.value
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
         setInputs({ ...inputs, [e.target.name]: value })
     }
 
 
     return (
         <>
-            <form>
+            <form className='form'>
                 <input type='text' name='fname' placeholder='First Name' onChange={(e) => handleChange(e)} />
                 <input type='text' name='sname' placeholder='Last Name' onChange={(e) => handleChange(e)} />
                 <input type='text' name='age' placeholder='Age' onChange={(e) => handleChange(e)} />
@@ -35,20 +37,22 @@ const Form = () => {
                     <option value="Odesa">Odesa</option>
                     <option value="New York">New York</option>
                 </select>
-                <input type="checkbox" name="alergic" value="Nuts" onChange={(e) => handleChange(e)} /> Nuts
-                <input type="checkbox" name="alergic" value="Lactose" onChange={(e) => handleChange(e)} /> Lactose
-                <input type="checkbox" name="alergic" value="VeganMeal" onChange={(e) => handleChange(e)} /> Vegan Meal
+                <input type="checkbox" name="dietary_nuts" value="Nuts" onChange={(e) => handleChange(e)} /> Nuts
+                <input type="checkbox" name="dietary_lactose" value="Lactose" onChange={(e) => handleChange(e)} /> Lactose
+                <input type="checkbox" name="dietary_veganmeal" value="VeganMeal" onChange={(e) => handleChange(e)} /> Vegan Meal
                 <input type='submit' />
             </form>
 
-            <div>
+            <div className='list'>
                 <ul>
                     <li>First Name: {inputs.fname}</li>
                     <li>Second Name:{inputs.sname}</li>
                     <li>Age:{inputs.age}</li>
                     <li>Gender:{inputs.gender}</li>
                     <li>Destination{inputs.destination}</li>
-                    <li>Alergic: {inputs.alergic}</li>
+                    <li>Nuts free: {inputs.dietary_nuts? 'ON':'OFF'}</li>
+                    <li>Lactose free: {inputs.dietary_lactose? 'ON' : 'OFF'}</li>
+                    <li>Vegan Meal: {inputs.dietary_veganmeal? 'ON': 'OFF'}</li>
                 </ul>
             </div>
         </>
